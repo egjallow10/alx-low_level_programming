b97d7828c318c602b345f3f78e7e9c7a8f66a4f0
egjallow10 <egjallow10@gmail.com>
2022-06-02 16:11:22 -0700

    use put statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main- main block
+ * Description:
+ * Should use function puts and print a string
+ * Return success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
