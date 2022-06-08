6c693a0aa9d8326ba6da6eac7afcf18943c1cad4
egjallow10 <egjallow10@gmail.com>
2022-06-07 17:39:19 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,14 @@
  *
  * Return: Always 0.
  */
-void  print_alphabet(void)
+void print_alphabet(void)
 {
-	char i;
+	char i = 'a';
 
-	for (i = 'a'; i <= 'z'; i++)
+	while (i <= 'z')
+	{
 		_putchar(i);
-	_putchar(10);
-	return (0);
+		i++;
+	}
+	_putchar('\n');
 }
