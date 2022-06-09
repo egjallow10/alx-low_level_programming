298680219d76510ca9e0affa9fa09720c0a3c7e0
egjallow10 <egjallow10@gmail.com>
2022-06-08 17:09:34 -0700

    funabaci 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even values in fibonacci sequence
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	double x = 1, y = 2, a, b = 2;
+	int z = 0;
+
+	while (y < 4000000)
+	{
+		a = y;
+		y += x;
+		x = a;
+		if (z == 2)
+		{
+			b += y;
+			z = 0;
+		}
+		else
+		{
+			z++;
+		}
+	}
+	printf("%.0f\n", b);
+	return (0);
+}
