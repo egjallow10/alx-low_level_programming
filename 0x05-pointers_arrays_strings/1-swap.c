28d939719b394e44163e43b311800f347d490864
egjallow10 <egjallow10@gmail.com>
2022-06-13 17:02:31 -0700

    Swap number

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * swap_int - swaps the value  integers
+ *
+ *  @a: firts init pointer
+ *  @b: second init pointer
+ *  Return void
+ */
+void swap_int(int *a, int *b)
+{
+	init x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
