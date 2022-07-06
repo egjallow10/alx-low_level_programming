65ba3aabede9b23ad369c898c691d22b307e79df
egjallow10 <egjallow10@gmail.com>
2022-07-06 13:36:37 -0700

    Function to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name
+ *
+ * @name: pointer to name to print
+ * @f: function to use to print name
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		name = malloc(sizeof(char *));
+		if (name == NULL)
+			return;
+	}
+	if (f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+0x0F. C - Function pointers
+In C, like normal data pointers (int *, char *, etc), we can have pointers to functions. Following is a simple example that shows declaration and function call using function pointer.
