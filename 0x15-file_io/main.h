470c4aceea27f3299477285a83728a2bbd762110
egjallow10 <egjallow10@gmail.com>
2022-07-25 11:54:28 -0700

    Created MAin,h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
