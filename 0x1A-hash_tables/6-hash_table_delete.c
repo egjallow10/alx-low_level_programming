6eaa48495d8579128b9d38c1c6d78916a45335c8
egjallo <egjallow10@gmail.com>
2022-09-23 22:57:10 +0000

    proto

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *next = NULL, *prev = NULL;
+	unsigned long int i = 0;
+
+	if (ht && ht->array && ht->size)
+	{
+		for (; i < ht->size; i++)
+		{
+			next = ht->array[i];
+			while (next)
+			{
+				prev = next;
+				next = next->next;
+				free(prev->key);
+				free(prev->value);
+				free(prev);
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
