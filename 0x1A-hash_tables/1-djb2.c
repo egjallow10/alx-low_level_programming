48c86f68a870b914159560deafe277e392bbdc12
egjallo <egjallow10@gmail.com>
2022-09-23 21:49:27 +0000

    djb2 alog

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - the djb2 algorithm
+ * @str: const string hey to be hashed
+ * Return: ulong hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381, c = 0;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+}
