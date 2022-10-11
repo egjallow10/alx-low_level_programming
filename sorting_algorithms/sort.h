29e64b03bbc208ab0503dd0f8532340a8aa13808
egjallo <egjallo@insistglobal.com>
2022-10-11 16:00:32 +0000

    done task 0

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -20,4 +20,5 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void swap_int(int *array, int index, size_t size);
+void bubble_sort(int *array, size_t size);
 #endif /*_SORT_H_*/
